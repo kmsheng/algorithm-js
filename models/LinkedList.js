9d2910b06488cf36b90918719373ae820d8cb748
kmsheng <kmsh3ng@gmail.com>
2021-10-09 16:37:57 +0800

    update LinkedList.js

--- models/LinkedList.js
+++ models/LinkedList.js
@@ -1,9 +1,41 @@
 class Node {
-
-  consturctor(value) {
+  constructor(value) {
     this.value = value
     this.next = null
   }
+}
+
+export const arrToList = arr => {
+  let head = new Node(null)
+  let node = head
+  arr.forEach(value => {
+    node.next = new Node(value)
+    node = node.next
+  })
+  return head.next
+}
+
+export const each = (head, fn) => {
+  let node = head
+  let i = 0
+  while (node) {
+    fn(node, i)
+    node = node.next
+    i++
+  }
+}
+
+export const log = head => {
+  let node = head
+  let str = ''
+  while (node) {
+    str += `${node.value}`
+    if (node.next) {
+      str += ` -> `
+    }
+    node = node.next
+  }
+  console.log(str)
 }
 
 export default Node
